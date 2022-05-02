462a806362cb69a23a03fa37723e4d47e2d4e100
programer1983 <programer972@gmail.com>
2022-05-02 15:39:21 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Adress</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jenny Chan</td>
+            <td>3 waterfoot road</td>
+            <td>333-962-7516</td>
+            <td>jenny.chan@email.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
